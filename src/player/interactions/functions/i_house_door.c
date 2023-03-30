818717a7026731a799d32eef5d60d32abb3bc434
LouisLanganay <louis.langanay@epitech.eu>
2023-03-30 23:56:34 +0200

    STYLE(player/interactions): Fixing codding style

--- src/player/interactions/functions/i_house_door.c
+++ src/player/interactions/functions/i_house_door.c
@@ -9,13 +9,15 @@
 
 void i_house_door(rpg_t *rpg)
 {
+    char *player_house = "resources/maps/player_house.json";
+    char *main_map = "resources/maps/main_map.json";
     if (sfKeyboard_isKeyPressed(sfKeyE) == sfTrue) {
-        if (my_strcmp(rpg->actual_map, "resources/maps/player_house.json") == 0) {
-            rpg->actual_map = "resources/maps/main_map.json";
+        if (my_strcmp(rpg->actual_map, player_house) == 0) {
+            rpg->actual_map = main_map;
             rpg->player->pos.x = SPAWN_OUT_HOUSE_X;
             rpg->player->pos.y = SPAWN_OUT_HOUSE_Y;
-        } else if (my_strcmp(rpg->actual_map, "resources/maps/main_map.json") == 0) {
-            rpg->actual_map = "resources/maps/player_house.json";
+        } else if (my_strcmp(rpg->actual_map, main_map) == 0) {
+            rpg->actual_map = player_house;
             rpg->player->pos.x = SPAWN_INTO_HOUSE_X;
             rpg->player->pos.y = SPAWN_INTO_HOUSE_Y;
         }
