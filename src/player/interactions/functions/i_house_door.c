e47280ed914c675cfdff49de5d218714d353c4b4
LouisLanganay <louis.langanay@epitech.eu>
2023-03-31 09:40:33 +0200

    STYLE(player/interactions): Fix codding style conditional branching

--- src/player/interactions/functions/i_house_door.c
+++ src/player/interactions/functions/i_house_door.c
@@ -7,20 +7,24 @@
 
 #include "rpg.h"
 
-void i_house_door(rpg_t *rpg)
+static void change_map(rpg_t *rpg)
 {
     char *player_house = "resources/maps/player_house.json";
     char *main_map = "resources/maps/main_map.json";
-    if (sfKeyboard_isKeyPressed(sfKeyE) == sfTrue) {
-        if (my_strcmp(rpg->actual_map, player_house) == 0) {
-            rpg->actual_map = main_map;
-            rpg->player->pos.x = SPAWN_OUT_HOUSE_X;
-            rpg->player->pos.y = SPAWN_OUT_HOUSE_Y;
-        } else if (my_strcmp(rpg->actual_map, main_map) == 0) {
-            rpg->actual_map = player_house;
-            rpg->player->pos.x = SPAWN_INTO_HOUSE_X;
-            rpg->player->pos.y = SPAWN_INTO_HOUSE_Y;
-        }
+    if (my_strcmp(rpg->actual_map, player_house) == 0) {
+        rpg->actual_map = main_map;
+        rpg->player->pos.x = SPAWN_OUT_HOUSE_X;
+        rpg->player->pos.y = SPAWN_OUT_HOUSE_Y;
+    } else if (my_strcmp(rpg->actual_map, main_map) == 0) {
+        rpg->actual_map = player_house;
+        rpg->player->pos.x = SPAWN_INTO_HOUSE_X;
+        rpg->player->pos.y = SPAWN_INTO_HOUSE_Y;
     }
+}
+
+void i_house_door(rpg_t *rpg)
+{
+    if (sfKeyboard_isKeyPressed(sfKeyE) == sfTrue)
+        change_map(rpg);
     while (sfKeyboard_isKeyPressed(sfKeyE) == sfTrue);
 }
