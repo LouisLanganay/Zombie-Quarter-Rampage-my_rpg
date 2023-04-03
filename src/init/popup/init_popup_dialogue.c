6691f86854622b87b2abad79c65b8f47798bff5f
LouisLanganay <louis.langanay@epitech.eu>
2023-04-03 18:31:58 +0200

    FEAT(player/interactions): add exa keyboard keys to array

--- src/init/popup/init_popup_dialogue.c
+++ src/init/popup/init_popup_dialogue.c
@@ -46,9 +46,23 @@ static void init_popup_dialogue_text(rpg_t *rpg)
     gl_create_text(rpg->glib, my_text);
 }
 
+static void init_popup_dialogue_text_escape(rpg_t *rpg)
+{
+    text_t *my_text = malloc(sizeof(text_t));
+    my_text->id = PLAYER_DIALOGUE_TEXT_ESCAPE;
+    my_text->pos = (sfVector2f){0, 0};
+    my_text->font = gl_get_font(rpg->glib, MINECRAFT_FONT);
+    my_text->scale = (sfVector2f){0.1, 0.1};
+    my_text->color = (sfColor){255, 255, 255, 255};
+    my_text->string = "";
+    my_text->size = 75;
+    gl_create_text(rpg->glib, my_text);
+}
+
 void init_popup_dialogue(rpg_t *rpg)
 {
     init_popup_dialogue_text(rpg);
     init_popup_dialogue_choice_one(rpg);
     init_popup_dialogue_choice_two(rpg);
+    init_popup_dialogue_text_escape(rpg);
 }
