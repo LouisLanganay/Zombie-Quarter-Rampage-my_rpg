ad5f7a05c863ebe4dd192a192dfaf2a23146e579
ValentinPeron <valentin.peron@epitech.eu>
2023-04-04 20:28:26 -0400

    FEAT(main): Basic mouvement && FEAT(main): shoot bullet && FEAT(main): Add angle with shoot && FEAT(main): Function to move bullet with an angle and speed && FEAT(main): Linked list function for struct bullets_t

--- combat/src/main.c
+++ combat/src/main.c
@@ -5,8 +5,9 @@
 ** main
 */
 
-#include "rpg.h"
+#include "../../combat/includes/rpg.h"
 #include "math.h"
+#define M_PI 3.14159265358979323846
 
 //1 = Background
 //2 = Player
@@ -70,57 +71,78 @@ void move_player(rpg_t *rpg, sfClock *clock)
     }
 }
 
-bullets_t *create_bullet(bullets_t *bullets, rpg_t *rpg, float speed)
+float shot_angle(sfVector2f pos, sfVector2i mouse)
 {
-    bullets_t *tmp = bullets;
-    if (bullets == NULL) {
-        bullets = malloc(sizeof(bullets_t));
-        bullets->pos = sfSprite_getPosition(gl_get_sprite(rpg->glib, 2));
-        bullets->speed = speed;
-        bullets->sprite = sfSprite_create();
-        bullets->texture = sfTexture_createFromFile("assets/01.png", NULL);
-        bullets->scale = (sfVector2f){1.0,1.0};
-        bullets->rect = (sfIntRect){0, 0, 100, 100};
-        bullets->next = NULL;
-        sfSprite_setTextureRect(bullets->sprite, bullets->rect);
-        sfSprite_setTexture(bullets->sprite, bullets->texture, sfTrue);
-        sfSprite_setScale(bullets->sprite, bullets->scale);
-        sfSprite_setPosition(bullets->sprite, bullets->pos);
-        return bullets;
-    }
-    else {
-    while (tmp->next != NULL) {
-        tmp = tmp->next;
-    }
-    tmp->next = malloc(sizeof(bullets_t));
-    tmp->next->pos = sfSprite_getPosition(gl_get_sprite(rpg->glib, 2));
-    tmp->next->speed = speed;
-    tmp->next->sprite = sfSprite_create();
-    tmp->next->texture = sfTexture_createFromFile("assets/bullet.png", NULL);
-    tmp->next->scale = (sfVector2f){1.25, 1.35};
-    tmp->next->rect = (sfIntRect){0, 0, 100, 100};
-    tmp->next->next = NULL;
-    sfSprite_setTextureRect(tmp->next->sprite, tmp->next->rect);
-    sfSprite_setTexture(tmp->next->sprite, tmp->next->texture, sfTrue);
-    sfSprite_setScale(tmp->next->sprite, tmp->next->scale);
-    sfSprite_setPosition(tmp->next->sprite, tmp->next->pos);
-    }
-    return bullets;
-
+    float angle = atan2(mouse.y - pos.y, mouse.x - pos.x) * 180 / M_PI;
+    return angle;
 }
 
-bullets_t *delete_outmap_bullet(bullets_t *bullets)
+void insert_bullet(bullets_t **list, rpg_t *rpg)
+{
+    bullets_t *new_node = malloc(sizeof(bullets_t));
+    new_node->speed = 10;
+    new_node->pos = sfSprite_getPosition(gl_get_sprite(rpg->glib, 2));
+    new_node->sprite = sfSprite_create();
+    new_node->texture = sfTexture_createFromFile("assets/bullet.png", NULL);
+    new_node->rect = (sfIntRect){0, 0, 100, 100};
+    new_node->scale = (sfVector2f){1.0, 1.0};
+    new_node->angle = shot_angle(new_node->pos, sfMouse_getPositionRenderWindow(rpg->glib->window->window));
+    new_node->next = *list;
+    sfSprite_setTexture(new_node->sprite, new_node->texture, sfTrue);
+    sfSprite_setTextureRect(new_node->sprite, new_node->rect);
+    sfSprite_setScale(new_node->sprite, new_node->scale);
+    sfSprite_setPosition(new_node->sprite, new_node->pos);
+    *list = new_node;
+}
+
+void delete_bullet(bullets_t **list, int index)
+{
+    bullets_t *tmp = *list;
+    bullets_t *prev = NULL;
+    int i = 0;
+    if (tmp == NULL)
+        return;
+    if (index == 0) {
+        *list = tmp->next;
+        free(tmp);
+        return;
+    }
+    while (tmp != NULL && i != index) {
+        prev = tmp;
+        tmp = tmp->next;
+        i++;
+    }
+    if (tmp == NULL)
+        return;
+    prev->next = tmp->next;
+    free(tmp);
+}
+
+void move_bullets(bullets_t *bullets)
 {
     bullets_t *tmp = bullets;
-    while (tmp->next != NULL) {
-        if (tmp->next->pos.x > 1920 && tmp->next->next == NULL) {
-                tmp->next = NULL;
-                return bullets;
-            }
-
+    while (tmp != NULL) {
+        tmp->pos.x += cos(tmp->angle * M_PI / 180) * tmp->speed;
+        tmp->pos.y += sin(tmp->angle * M_PI / 180) * tmp->speed;
+        sfSprite_setPosition(tmp->sprite, tmp->pos);
         tmp = tmp->next;
     }
-    return bullets;
+}
+
+void delete_outmap_bullet(bullets_t **bullets)
+{
+    bullets_t *tmp = *bullets;
+    if (tmp == NULL)
+        return;
+    int i = 0;
+    while (tmp != NULL) {
+        if (tmp->pos.x > 1920 || tmp->pos.x < 0 || tmp->pos.y > 1080 || tmp->pos.y < 0) {
+            delete_bullet(bullets, i);
+            return;
+        }
+        tmp = tmp->next;
+        i++;
+    }
 }
 
 void print_len_list(bullets_t *bullets)
@@ -138,7 +160,7 @@ void delete_element(bullets_t *bullets, int index)
 {
     bullets_t *tmp = bullets;
     int i = 0;
-    while (tmp != NULL) {
+    while (tmp->next != NULL) {
         if (i == index) {
             tmp->next = tmp->next->next;
             return;
@@ -148,33 +170,15 @@ void delete_element(bullets_t *bullets, int index)
     }
 }
 
-void hit_boss(rpg_t *rpg, bullets_t *bullets)
+void draw_bullets(bullets_t *bullets, rpg_t *rpg)
 {
-    sfVector2f sprite_pos = sfSprite_getPosition(gl_get_sprite(rpg->glib, 0));
-    sfVector2f sprite_size = sfSprite_getScale(gl_get_sprite(rpg->glib, 0));
-    sfVector2f top_right = (sfVector2f){sprite_pos.x, sprite_pos.y};
-    sfVector2f bottom_left = (sfVector2f){sprite_pos.x + sprite_size.x * 100, sprite_pos.y + sprite_size.y * 100};
-
-    printf("sprite top right = %f, %f\n", top_right.x, top_right.y);
-    printf("sprite bottom left = %f, %f\n", bottom_left.x, bottom_left.y);
-    void *tmp = bullets;
-    //SPRITE IS reversed
-
-    //printf("sprite top left = %f, %f\n", sprite_pos.x, sprite_pos.y);
-    //printf("sprite bottom right = %f, %f", sprite_pos.x + sprite_size.x * 100, sprite_pos.y + sprite_size.y * 100);
-    //printf("sprite size = %f, %f\n", sprite_size.x, sprite_size.y);
-    //bullets_t *tmp = bullets;
-    //int i = 0;
-    //while (tmp != NULL) {
-    //    if (tmp->pos.x > sprite_pos.x && tmp->pos.x < sprite_pos.x + sprite_size.x * 100) {
-    //        if (tmp->pos.y > sprite_pos.y && tmp->pos.y < sprite_pos.y + sprite_size.y * 100) {
-    //            delete_element(bullets, i);
-    //            printf(":::hit\n");
-    //        }
-    //    }
-    //    i++;
-    //    tmp = tmp->next;
-    //}
+    bullets_t *tmp = bullets;
+    if (tmp == NULL)
+        return;
+    while (tmp != NULL) {
+        sfRenderWindow_drawSprite(rpg->glib->window->window, tmp->sprite, NULL);
+        tmp = tmp->next;
+    }
 }
 
 void hit_by_bullet(sfSprite *sprite, bullets_t *bullets)
@@ -197,99 +201,13 @@ void collision(sfRectangleShape *hitbox, bullets_t *bullets)
     bullets_t *tmp = bullets;
     int i = 0;
     while (tmp != NULL) {
-        if (click_in_rect(hitbox, tmp->pos) == 1) {
+        if (colision_with_rect(hitbox, tmp->pos) == 1) {
             delete_element(bullets, i);
             printf(":::hit\n");
         }
         tmp = tmp->next;
         i++;
     }
-}
-
-    // if (rect_scale.x > 0 && rect_scale.y < 0) {
-    //     if (pos.x > rect_pos.x && pos.x < rect_pos.x + rect_scale.x * 100) {
-    //         if (pos.y < rect_pos.y && pos.y > rect_pos.y + rect_scale.y * 100) {
-    //             printf("click in rect\n");
-    //         }
-    //     }
-    // }
-    // //if scale x is negative and y is positive
-    // if (rect_scale.x < 0 && rect_scale.y > 0) {
-    //     if (pos.x < rect_pos.x && pos.x > rect_pos.x + rect_scale.x * 100) {
-    //         if (pos.y > rect_pos.y && pos.y < rect_pos.y + rect_scale.y * 100) {
-    //             printf("click in rect\n");
-    //         }
-    //     }
-    // }
-    // //if scale x is positive and y is positive
-    // if (rect_scale.x > 0 && rect_scale.y > 0) {
-    //     if (pos.x > rect_pos.x && pos.x < rect_pos.x + rect_scale.x * 100) {
-    //         if (pos.y > rect_pos.y && pos.y < rect_pos.y + rect_scale.y * 100) {
-    //             printf("click in rect\n");
-    //         }
-    //     }
-    // }
-    // //if scale x is negative and y is negative
-    // if (rect_scale.x < 0 && rect_scale.y < 0) {
-    //     if (pos.x < rect_pos.x && pos.x > rect_pos.x + rect_scale.x * 100) {
-    //         if (pos.y < rect_pos.y && pos.y > rect_pos.y + rect_scale.y * 100) {
-    //             printf("click in rect\n");
-    //         }
-    //     }
-    // }
-
-int check_colision_1(sfVector2f rect_pos, sfVector2f rect_scale, sfVector2f pos)
-{
-    if (pos.x > rect_pos.x && pos.x < rect_pos.x + rect_scale.x * 100) {
-        if (pos.y < rect_pos.y && pos.y > rect_pos.y + rect_scale.y * 100) {
-            return (1);
-        }
-    }
-    return (0);
-}
-
-int check_colision_2(sfVector2f rect_pos, sfVector2f rect_scale, sfVector2f pos)
-{
-    if (pos.x < rect_pos.x && pos.x > rect_pos.x + rect_scale.x * 100) {
-        if (pos.y > rect_pos.y && pos.y < rect_pos.y + rect_scale.y * 100) {
-            return (1);
-        }
-    }
-    return (0);
-}
-
-int check_colision_3(sfVector2f rect_pos, sfVector2f rect_scale, sfVector2f pos)
-{
-    if (pos.x > rect_pos.x && pos.x < rect_pos.x + rect_scale.x * 100) {
-        if (pos.y > rect_pos.y && pos.y < rect_pos.y + rect_scale.y * 100) {
-            return (1);
-        }
-    }
-    return (0);
-}
-
-int check_colision_4(sfVector2f rect_pos, sfVector2f rect_scale, sfVector2f pos)
-{
-    if (pos.x < rect_pos.x && pos.x > rect_pos.x + rect_scale.x * 100) {
-        if (pos.y < rect_pos.y && pos.y > rect_pos.y + rect_scale.y * 100) {
-            return (1);
-        }
-    }
-    return (0);
-}
-
-int  click_in_rect(sfRectangleShape *rect, sfVector2f pos)
-{
-    sfVector2f rect_pos = sfRectangleShape_getPosition(rect);
-    sfVector2f rect_scale = sfRectangleShape_getScale(rect);
-    if (rect_scale.x > 0 && rect_scale.y < 0)
-        return (check_colision_1(rect_pos, rect_scale, pos));
-    if (rect_scale.x < 0 && rect_scale.y > 0)
-        return (check_colision_2(rect_pos, rect_scale, pos));
-    if (rect_scale.x > 0 && rect_scale.y > 0)
-        return (check_colision_3(rect_pos, rect_scale, pos));
-    if (rect_scale.x < 0 && rect_scale.y < 0)
-        return (check_colision_4(rect_pos, rect_scale, pos));
 }
 
 void init_boss(rpg_t *rpg)
@@ -314,22 +232,8 @@ int main(int ac, char **av)
     gl_create_event(rpg->glib, 50, sfEvtClosed, e_close);
     sfClock *clock = sfClock_create();
     sfClock *clock_shoot = sfClock_create();
-    bullets_t *bullets = malloc(sizeof(bullets_t));
-    bullets->pos = sfSprite_getPosition(gl_get_sprite(rpg->glib, 2));
-    bullets->speed = 10;
-    bullets->sprite = sfSprite_create();
-    bullets->texture = sfTexture_createFromFile("assets/bullet.png", NULL);
-    bullets->scale = (sfVector2f){1.25, 1.35};
-    bullets->rect = (sfIntRect){0, 0, 100, 100};
-    bullets->next = NULL;
-    //delete_element(bullets, 0);
-    /*
-    sfVector2f sprite_pos = sfSprite_getPosition(gl_get_sprite(rpg->glib, 0));
-    sfVector2f sprite_size = sfSprite_getScale(gl_get_sprite(rpg->glib, 0));
-    sfVector2f top_right = (sfVector2f){sprite_pos.x, sprite_pos.y};
-    sfVector2f bottom_left = (sfVector2f){sprite_pos.x + sprite_size.x * 100, sprite_pos.y + sprite_size.y * 100};
-    */
-    //display collision box
+    bullets_t *bullets = NULL;
+
     sfRectangleShape *rect = sfRectangleShape_create();
     sfRectangleShape_setPosition(rect, (sfVector2f){1500, 300});
     sfRectangleShape_setSize(rect, (sfVector2f){100, 100});
@@ -340,36 +244,34 @@ int main(int ac, char **av)
 
 
 
-
     while (sfRenderWindow_isOpen(rpg->glib->window->window)) {
         print_framerate();
         sfRenderWindow_clear(rpg->glib->window->window, sfBlack);
         gl_draw_sprite(rpg->glib, 1);
         gl_draw_sprite(rpg->glib, 0);
-        gl_draw_sprite(rpg->glib, 2);
         move_player(rpg, clock);
-        if (sfKeyboard_isKeyPressed(sfKeySpace) && sfClock_getElapsedTime(clock_shoot).microseconds / 200000.0 > 1) {
-            bullets = create_bullet(bullets, rpg, 10);
+        if (sfMouse_isButtonPressed(sfMouseLeft) && sfClock_getElapsedTime(clock_shoot).microseconds / 200000.0 > 1) {
+            //bullet_insert(bullets, rpg);
+            insert_bullet(&bullets, rpg);
+            print_len_list(bullets);
             sfClock_restart(clock_shoot);
         }
-        bullets_t *tmp = bullets;
-        while (tmp->next != NULL) {
-            tmp = tmp->next;
-            tmp->pos.x += tmp->speed;
-            sfSprite_setPosition(tmp->sprite, tmp->pos);
-            sfRenderWindow_drawSprite(rpg->glib->window->window, tmp->sprite, NULL);
-        }
-        if (sfMouse_isButtonPressed(sfMouseLeft)) {
-            sfVector2i mouse_pos = sfMouse_getPositionRenderWindow(rpg->glib->window->window);
-            sfVector2f mouse_pos_f = (sfVector2f){mouse_pos.x, mouse_pos.y};
-            click_in_rect(rect, mouse_pos_f);
-        }
+        draw_bullets(bullets, rpg);
+        gl_draw_sprite(rpg->glib, 2);
+        move_bullets(bullets);
+        
+        //if (sfMouse_isButtonPressed(sfMouseLeft)) {
+        //    sfVector2i mouse_pos = sfMouse_getPositionRenderWindow(rpg->glib->window->window);
+        //    sfVector2f mouse_pos_f = (sfVector2f){mouse_pos.x, mouse_pos.y};
+        //    //click_in_rect(rect, mouse_pos_f);
+        //}
         //bullets = delete_outmap_bullet(bullets);
         //hit_boss(rpg, bullets);
         sfRenderWindow_drawRectangleShape(rpg->glib->window->window, rect, NULL);
-        collision(rect, bullets);
+        //collision(rect, bullets);
         //hit_by_bullet(gl_get_sprite(rpg->glib, 0), bullets);
         //print_len_list(bullets);
+        delete_outmap_bullet(&bullets);
         gl_check_events(rpg->glib->window, rpg->glib->events, rpg);
         sfRenderWindow_display(rpg->glib->window->window);
     }
