67f3e9ae6b9b60e58cc7b5ffbc956d16a02f3b04
LouisLanganay <louis.langanay@epitech.eu>
2023-04-05 10:53:10 +0200

    FEAT(src): add start game system, can use start_game function to start game

--- src/main.c
+++ src/main.c
@@ -17,18 +17,19 @@ int main(int ac, char **av)
 {
     rpg_t *rpg = malloc(sizeof(rpg_t));
     init_rpg(rpg, ac, av);
-    create_map(rpg, "resources/maps/main_map.json");
-    create_map(rpg, "resources/maps/player_house.json");
     init(rpg);
-    load_save(rpg, "resources/save.json");
 
     while (sfRenderWindow_isOpen(rpg->glib->window->window)) {
         print_framerate();
         sfRenderWindow_clear(rpg->glib->window->window, sfBlack);
-        set_view_on_player(rpg);
-        gl_check_events(rpg->glib->window, rpg->glib->events, rpg);
-        draw_map(rpg->maps, rpg->actual_map, rpg);
-        display_dialogue(rpg);
+        if (rpg->game_started == 1)
+            game_loop(rpg);
+        else {
+            gl_check_events(rpg->glib->window, rpg->glib->events, rpg);
+            draw_splash_screen(rpg);
+        }
         sfRenderWindow_display(rpg->glib->window->window);
+        if (rpg->maps_loaded == 0)
+            load_maps(rpg);
     }
 }
