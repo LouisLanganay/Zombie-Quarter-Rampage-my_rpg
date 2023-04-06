69e4bdd671b693051e16ca7d070ee9f997c77f89
LouisLanganay <louis.langanay@epitech.eu>
2023-04-06 14:34:27 +0200

    FIX(Inventory): Fix inventory initialization

--- src/init/inventory/init_inventory.c
+++ src/init/inventory/init_inventory.c
@@ -15,8 +15,6 @@ void init_inventory(rpg_t *rpg)
     rpg->player->inventory->background = sfRectangleShape_create();
     rpg->player->inventory->items_sprite = malloc(sizeof(sfSprite *) *
     INVENTORY_SIZE);
-    rpg->player->inventory->bg_items_sprite = malloc(sizeof(sfSprite *) *
-    INVENTORY_UI_SIZE);
     for (int i = 0; i < INVENTORY_SIZE; i++)
         rpg->player->inventory->items_sprite[i] = sfSprite_create();
     rpg->player->inventory->bg_items_sprite = sfSprite_create();
