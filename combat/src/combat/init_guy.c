a2041af5a5cda1e22b06fdcc1b2fe8dc490cd567
ValentinPeron <valentin.peron@epitech.eu>
2023-04-06 10:18:00 -0400

    FIX(init_guy): update init_guy

--- combat/src/combat/init_guy.c
+++ combat/src/combat/init_guy.c
@@ -10,12 +10,14 @@
 void init_guy(rpg_t *rpg)
 {
     sprite_t *my_sprite = malloc(sizeof(sprite_t));
+
     my_sprite->id = 2;
     my_sprite->pos = (sfVector2f){200, 450};
     my_sprite->sprite = sfSprite_create();
     my_sprite->texture = sfTexture_createFromFile("assets/guy.png", NULL);
     my_sprite->scale = (sfVector2f){1.5, 1.5};
     my_sprite->call_action = NULL;
+    my_sprite->next = NULL;
     sfSprite_setOrigin(my_sprite->sprite, (sfVector2f){100, -20});
     gl_create_sprite(rpg->glib, my_sprite);
 }
