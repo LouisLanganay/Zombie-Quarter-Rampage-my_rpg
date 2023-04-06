ca450c96a653b041d168607f723719a0e8bfadce
ValentinPeron <valentin.peron@epitech.eu>
2023-04-06 07:18:07 -0400

    FEAT(draw_zombies):draw zombies

--- combat/src/zombies/draw_zombies.c
+++ combat/src/zombies/draw_zombies.c
@@ -13,6 +13,14 @@ void draw_zombies(zombies_t *list, rpg_t *rpg)
     if (tmp == NULL)
         return;
     while (tmp != NULL) {
+        if (tmp->direction == 1) {
+            sfSprite_setScale(tmp->sprite, (sfVector2f){-2, 2});
+            sfSprite_setPosition(tmp->sprite, (sfVector2f){tmp->pos.x, tmp->pos.y});
+        }
+        else {
+            sfSprite_setScale(tmp->sprite, (sfVector2f){2, 2});
+            sfSprite_setPosition(tmp->sprite, (sfVector2f){tmp->pos.x - 152, tmp->pos.y});
+        }
         sfRenderWindow_drawSprite(rpg->glib->window->window, tmp->sprite, NULL);
         sfRenderWindow_drawRectangleShape(rpg->glib->window->window, tmp->hitbox, NULL);
         tmp = tmp->next;
