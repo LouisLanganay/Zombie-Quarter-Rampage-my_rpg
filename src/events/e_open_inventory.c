927d88ea908b4ec4eb5476dac651aae1ad75ed8f
Blaizot <guillaume.blaizot@epitech.eu>
2023-04-06 14:21:46 +0200

    STYLE(events): coding-style

--- src/events/e_open_inventory.c
+++ src/events/e_open_inventory.c
@@ -11,12 +11,11 @@ void e_open_inventory(window_t *window, void *main)
 {
     (void)window;
     rpg_t *rpg = (rpg_t *)main;
-    if (sfKeyboard_isKeyPressed(rpg->player->keys->inventory.key) == sfFalse) return;
 
-    if (rpg->player->inventory->is_open == 0) {
-       rpg->player->inventory->is_open = 1;
-    } else {
-       rpg->player->inventory->is_open = 0;
-    }
-
+    if (sfKeyboard_isKeyPressed(rpg->player->keys->inventory.key) == sfFalse)
+        return;
+    if (rpg->player->inventory->is_open == 0)
+        rpg->player->inventory->is_open = 1;
+    else
+        rpg->player->inventory->is_open = 0;
 }
