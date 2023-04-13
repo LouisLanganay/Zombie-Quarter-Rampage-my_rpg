2df2470d3fa2bc66297351fc26ec0db78ef38571
Blaizot <guillaume.blaizot@epitech.eu>
2023-04-13 21:57:07 +0200

    FEAT(inventory): add an command_bind to rm items

--- src/events/e_open_inventory.c
+++ src/events/e_open_inventory.c
@@ -29,4 +29,9 @@ void e_open_inventory(window_t *window, void *main)
     rpg->glib->window->event.key.code == sfKeyRight ||
     rpg->glib->window->event.key.code == sfKeyLeft)
         rpg->player->inventory->is_data_open = 1;
+    if (sfKeyboard_isKeyPressed(rpg->player->keys->choice_one.key) == sfTrue
+    && rpg->player->inventory->is_data_open == 0) {
+        rpg->player->inventory->is_data_open = 1;
+        remove_item_to_inventory(rpg, RP->inventory->pos);
+    }
 }
