c1298c02d4b9e4a10750fd5df54ec1eaee8627ae
Augustin Bost <augustin.bost@epitech.eu>
2023-04-15 00:05:12 +0200

    FEAT(e_lang_en.c && init_background): init of background and event of english language

---
+++ src/init/menu/init_background.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** B-MUL-200-REN-2-1-myrpg-louis.langanay
+** File description:
+** init_background.c
+*/
+
+#include "rpg.h"
+
+void init_background(rpg_t *rpg)
+{
+    sfImage *image = sfImage_createFromFile("resources/assets/background/background_1.jpg");
+    sfTexture *texture = sfTexture_createFromImage(image, NULL);
+    rpg->background->active = false;
+    rpg->background->background_menu_s = sfSprite_create();
+
+    sfSprite_setTexture(rpg->background->background_menu_s, texture, sfTrue);
+}
