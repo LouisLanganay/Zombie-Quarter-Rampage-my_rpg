dcab51dd368092b4623a8d86478c640109e8458d
Augustin Bost <augustin.bost@epitech.eu>
2023-04-14 12:09:46 +0200

    FEAT(e_langue): event of change language

---
+++ src/events/e_langue.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2022
+** B-MUL-200-REN-2-1-myrpg-louis.langanay
+** File description:
+** e_langue.c
+*/
+
+#include "rpg.h"
+
+void e_langue_fr(int id, void *main)
+{
+    (void)id;
+    rpg_t *rpg = (rpg_t *)main;
+    rpg->settings->game_language = FR;
+}
+
+void e_langue_en(int id, void *main)
+{
+    (void)id;
+    rpg_t *rpg = (rpg_t *)main;
+    rpg->settings->game_language = EN;
+}
