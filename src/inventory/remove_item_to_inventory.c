d2be41ff2498945a646dc7284c52a6db33b9af67
LouisLanganay <louis.langanay@epitech.eu>
2023-04-16 00:14:08 +0200

    FIX(inventory): fix item name

--- src/inventory/remove_item_to_inventory.c
+++ src/inventory/remove_item_to_inventory.c
@@ -12,13 +12,14 @@ int remove_item_to_inventory(rpg_t *rpg, int pos)
     if (RPI->items[pos] == -1)
         return -1;
     sfVector2f poss = sfView_getCenter(RP->view->view);
-    RPI->popup->item_name = my_strdup(get_item_name(RPI->items[pos]));
+    RPI->popup->item_name = get_language(rpg,
+        get_item_name(RPI->items[pos]), RSG);
     RPI->popup->action = REMOVE;
     int mid_char = get_mid_char(RPI->popup->item_name);
     sfText *text = gl_get_text(rpg->glib, INVENTORY_ITEM_POPUP);
 
     sfText_setPosition(text,
-       (sfVector2f){poss.x - mid_char * 2.65, poss.y + 160});
+        (sfVector2f){poss.x - mid_char * 2.65, poss.y + 160});
     sfClock_restart(RPI->popup->clock);
     sfClock_restart(RPI->popup->clock2);
     RPI->items[pos] = -1;
