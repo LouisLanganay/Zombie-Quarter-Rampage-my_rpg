729a7dfe46e2cc7e8f565a16b19ae6484635e6a4
Blaizot <guillaume.blaizot@epitech.eu>
2023-04-17 16:01:36 +0200

    FEAT(inventory): rm sound when item cant be rm

--- src/inventory/remove_item_to_inventory.c
+++ src/inventory/remove_item_to_inventory.c
@@ -26,6 +26,7 @@ int remove_item_to_inventory(rpg_t *rpg, int pos)
     }
     if (RPI->items[pos] == -1)
         return -1;
+    gl_play_sound(rpg->glib, DROP_SOUND_ID);
     add_item_popup(rpg, RPI->items[pos], REMOVE);
     RPI->items[pos] = -1;
     return 0;
