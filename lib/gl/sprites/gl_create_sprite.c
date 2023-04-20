053d70e291ce00157e6ea602c8e3fd5bc2654d73
ValentinPeron <valentin.peron@epitech.eu>
2023-04-20 11:31:23 -0400

    FIX(gl_create_sprite): fix segfault

--- lib/gl/sprites/gl_create_sprite.c
+++ lib/gl/sprites/gl_create_sprite.c
@@ -20,8 +20,6 @@ static int gl_create_sprite_check_id(sprite_t *sprites, int id)
 
 int gl_create_sprite(GLib_t *glib, sprite_t *sprite)
 {
-    if (gl_create_sprite_check_id(glib->sprites, sprite->id) != 0)
-        return (84);
     sprite_t *tmp = malloc(sizeof(*tmp));
     tmp->id = sprite->id;
     tmp->pos = sprite->pos;
